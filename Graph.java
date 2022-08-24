e416fd38e887884dc45e2c39a62231f70d6ee5d4
Luis Guilherme <49692250+LGuilhermeAg@users.noreply.github.com>
2022-08-24 23:32:57 +0000

    buscaProfundidade 24/8

--- Graph.java
+++ Graph.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.Stack;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Graph {
   private int countNodes;
   private int countEdges;
@@ -6,6 +13,29 @@ public class Graph {
   public Graph(int countNodes) {
     this.countNodes = countNodes;
     this.adjMatrix = new int[countNodes][countNodes];
+  }
+
+  public Graph(String fileName) throws IOException {
+    File file = new File(fileName);
+    FileReader reader = new FileReader(file);
+    BufferedReader bufferedReader = new BufferedReader(reader);
+
+    // Read header
+    String[] line = bufferedReader.readLine().split(" ");
+    this.countNodes = (Integer.parseInt(line[0]));
+    int fileLines = (Integer.parseInt(line[1]));
+
+    // Create and fill adjMatrix with read edges
+    this.adjMatrix = new int[this.countNodes][this.countNodes];
+    for (int i = 0; i < fileLines; ++i) {
+      String[] edgeInfo = bufferedReader.readLine().split(" ");
+      int source = Integer.parseInt(edgeInfo[0]);
+      int sink = Integer.parseInt(edgeInfo[1]);
+      int weight = Integer.parseInt(edgeInfo[2]);
+      addEdges(source, sink, weight);
+    }
+    bufferedReader.close();
+    reader.close();
   }
 
   public int getCountNodes() {
@@ -131,29 +161,95 @@ public class Graph {
   }
 
   /*
-pseudocodigo busca por largura em grafos
-maiusculo significa conjunto, minusculo valor||variavel
-origem: breath-first search (BFS) - algoritmo
+   * 17/8
+   * pseudocodigo busca por largura em grafos
+   * maiusculo significa conjunto, minusculo valor||variavel
+   * origem: breath-first search (BFS) - algoritmo
+   * 
+   * busca largura(g=(V, E), s){ //recebe um grafo representado como conjunto de
+   * vertices e arestas e um vertice origem
+   * para cada v em V{ //para cada vertice
+   * desc[v]=0 //vetor binário de vértices descobertos ou não descobertos
+   * Q={s} //cria dois conjuntos com um elemento s(valor da origem): fila (FIFO)
+   * de nós a processar
+   * R={s} //ordem da descoberta (armazena a sequencia de nos processados)
+   * desc[s]=1 //o nó origem foi descoberto
+   * enquanto q!=vazio{ //Enquanto houver nós a processar
+   * u=remove primeiro elemento de Q //remove o primeiro elemento da fila e salva
+   * em u (Q={} u=s)
+   * para cada v adjacente a u //proximo nivel de nós adjacentes (conectados por
+   * arestas a u)
+   * se desc[v]=0{ //novo elemento descoberto
+   * adcione v ao final de Q //v é mais um elemento Q={v¹,v²,v³...}
+   * adcione v ao final de R //v é mais um elemento processado R={s,v¹,v²,v³...}
+   * desc[v]=1 //v foi descoberto
+   * }
+   * } //retorna para o while processando a fila composta pelos elementos do
+   * proximo nivel
+   * }
+   * } //queue termina vazia, R termina cheio e desc termina com todas posicoes =
+   * 1
+   * retorna R //retorna a ordem de descoberta dos nós
+   * }
+   * 22/8
+   */
+  public ArrayList<Integer> buscaLargura(int s) {
 
-busca largura(g=(V, E), s){            //recebe um grafo representado como conjunto de vertices e arestas e um vertice origem 
-  para cada v em V{                    //para cada vertice
-    desc[v]=0                          //vetor binário de vértices descobertos ou não descobertos
-    Q={s}                              //cria dois conjuntos com um elemento s(valor da origem): fila (FIFO) de nós a processar
-    R={s}                              //ordem da descoberta (armazena a sequencia de nos processados)
-    desc[s]=1                          //o nó origem foi descoberto
-    enquanto q!=vazio{                 //Enquanto houver nós a processar
-      u=remove primeiro elemento de Q  //remove o primeiro elemento da fila e salva em u (Q={} u=s)
-      para cada v adjacente a u        //proximo nivel de nós adjacentes (conectados por arestas a u)
-        se desc[v]=0{                  //novo elemento descoberto
-          adcione v ao final de Q      //v é mais um elemento Q={v¹,v²,v³...}
-          adcione v ao final de R      //v é mais um elemento processado R={s,v¹,v²,v³...}
-          desc[v]=1                    //v foi descoberto
-        }                
-      }                                //retorna para o while processando a fila composta pelos elementos do proximo nivel 
+    int[] desc = new int[this.countNodes];
+
+    ArrayList<Integer> q = new ArrayList<>();
+    ArrayList<Integer> r = new ArrayList<>();
+    q.add(s);
+    r.add(s);
+    desc[s] = 1;
+
+    while (q.size() > 0) {
+      int u = q.remove(0);
+      for (int v = 0; v < this.adjMatrix[u].length; v++) {
+        if (this.adjMatrix[u][v] != 0) {
+          if (desc[v] == 0) {
+            q.add(v);
+            r.add(v);
+            desc[v] = 1;
+          }
+
+        }
+      }
     }
-  }                                    //queue termina vazia, R termina cheio e desc termina com todas posicoes = 1
-  retorna R                            //retorna a ordem de descoberta dos nós
-}
-*/
+    return r;
+  }
+
+  public boolean connected() {
+    return this.buscaLargura(0).size() == this.countNodes;
+  }
+
+  // 24/8 - depth-first-search | DFS
+  public ArrayList<Integer> buscaProfundidade(int s) {
+
+    int[] desc = new int[this.countNodes];
+
+    Stack<Integer> stack = new Stack<>();
+    ArrayList<Integer> r = new ArrayList<>();
+    stack.add(s);
+    r.add(s);
+    desc[s] = 1;
+
+    while (stack.size() != 0) {
+      int u = stack.get(stack.size() - 1);
+      boolean desempilhar = true;
+      for (int v = 0; v < this.adjMatrix[u].length; v++) {
+        if (this.adjMatrix[u][v] != 0 && desc[v] == 0) {
+          stack.add(v);
+          r.add(v);
+          desc[v] = 1;
+          desempilhar = false;
+          break;
+        }
+      }
+      if (desempilhar)
+        stack.remove(stack.size() - 1);
+    }
+    return r;
+  }
 
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Graph g1 = new Graph(4);
     System.out.println(g1);
     g1.addEdges(0, 1, 3);
@@ -7,11 +13,18 @@ class Main {
     g1.addEdges(0, 3, 4);
     g1.addEdges(3, 0, 4);
     System.out.println(g1);
-    Graph g2=g1.complement();
+    Graph g2 = g1.complement();
     System.out.println(g2);
-    System.out.println("Nó de maior grau: "+g1.highestNode()+" de grau "+g1.highestDegree());
-    System.out.println("Menor grau: "+g1.lowestDegree());
-    System.out.println("Densidade: "+g1.density());
-    
+    System.out.println("Nó de maior grau: " + g1.highestNode() + " de grau " + g1.highestDegree());
+    System.out.println("Menor grau: " + g1.lowestDegree());
+    System.out.println("Densidade: " + g1.density() + "\n\n");
+
+    String path = "graph.txt";
+    Graph g3 = new Graph(path);
+    System.out.println(g3);
+
+    ArrayList<Integer> r = new ArrayList<>();
+    r=g3.buscaProfundidade(0);
+    System.out.println(r);
   }
 }
